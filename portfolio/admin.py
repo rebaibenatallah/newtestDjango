5481fc475260642b73040360cf586303317cd169
rebaibenatallah <benatallahrebai@gmail.com>
2024-09-20 00:58:43 +0200

    models create table skills and templates portfolio

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import User_portfolio
+from .models import Skills
 # Register your models here.
 admin.site.register(User_portfolio)
+admin.site.register(Skills)
--- portfolio/models.py
+++ portfolio/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 class User_portfolio(models.Model):
-    
     Name = models.CharField(max_length=250)
     Job = models.CharField(max_length=250)
-    Years_work = models.IntegerField(max_length=5)
-    Completed_projects = models.IntegerField(max_length=5)
-    Satisfied_costomers = models.IntegerField(max_length=5)
+    image = models.ImageField(upload_to='photos/%y/%m/%d')
+    Years_work = models.IntegerField()
+    Completed_projects = models.IntegerField()
+    Satisfied_costomers = models.IntegerField()
 
+# content = models.TextField()
+# price = models.DecimalField(max_digits=5 , decimal_places=2) 
+# image = models.ImageField(upload_to='photos/%y/%m/%d')
+
+class Skills(models.Model):
+    Name = models.CharField(max_length=255)
